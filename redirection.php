4fae35ae647e328f87f083b479a04cb8b934e005
aletexis-PROME <aescubilla@provinciamicrocreditos.com>
2023-07-31 12:06:52 -0300

    rollback redirección

--- redirection.php
+++ redirection.php
@@ -45,14 +45,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script type="text/javascript">
-        // $(document).ready(function() {
-        //     setTimeout(function() {
-        //         document.getElementById('link').click();
-        //     }, 2000);
-        // });
         $(document).ready(function() {
             setTimeout(function() {
-                document.getElementById('Cursos').getElementById('link').click();
+                document.getElementById('link').click();
             }, 2000);
         });
     </script>
